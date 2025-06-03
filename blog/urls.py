a4b3a184308ebca193b1a3f31de83af32e84fcda
Xiayucheng <xiayucheng2@gmail.com>
2025-06-03 14:03:25 +0900

    增加跳转

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-# blog/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),  # 文章列表页
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),  # 文章详情页
 ]
