5160d0449a01279d576d553e6579feb4cba4a195
Xiayucheng <xiayucheng2@gmail.com>
2025-06-03 10:38:03 +0900

    Add template and CSS design for blog

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
